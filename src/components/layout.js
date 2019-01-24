982217864d242d81b607544bfcec22a6630f7881
Narin Sundarabhaya <nrs710@gmail.com>
2019-01-24 07:46:49 -0800

    feature: re-write main layout and add react-emotion

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,37 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
-
-import Header from './header'
 import './layout.css'
+
+import styled from '@emotion/styled'
+import { graphql, StaticQuery } from 'gatsby'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+import { colors, heights } from '../styles/variables'
+import Header from './header'
+
+const RootContainer = styled('div')`
+  display: grid;
+  grid-template-rows: ${heights.header}px max-content ${heights.footer}px;
+  grid-template-columns: 1fr;
+`
+
+const MainBodyContainer = styled('div')`
+  display: grid;
+  height: calc(100vh - ${heights.header}px - ${heights.footer}px);
+  width: 100vw;
+  margin: 0 auto;
+  padding: 0px 1.0875rem 1.45rem;
+  grid-row: 2;
+`
+
+const FooterContainer = styled('footer')`
+  display: grid;
+  grid-template-columns: max-content max-content;
+  grid-column-gap: 10px;
+  align-items: center;
+  justify-content: center;
+  grid-row: 3 / -1;
+  background-color: ${colors.header};
+`
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -17,24 +45,13 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
+      <RootContainer>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          {children}
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
+        <MainBodyContainer>{children}</MainBodyContainer>
+        <FooterContainer>
+          © {new Date().getFullYear()}, <span>Narin R. Sundarabhaya</span>
+        </FooterContainer>
+      </RootContainer>
     )}
   />
 )
