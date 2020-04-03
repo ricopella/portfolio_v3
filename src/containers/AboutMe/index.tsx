b8707d0c6bb7757af0bcbe117a763a6f7bd6bde4
Narin Sundarabhaya <nrs710@gmail.com>
2020-04-02 22:19:12 -0700

    feature(AboutMe): add ToggleSwitch and ProfileImage

--- src/containers/AboutMe/index.tsx
+++ src/containers/AboutMe/index.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Page from '../../components/Page/index'
 import TerminalCommand from '../../components/TerminalCommand/index'
 import Styled from './AboutMe.styles'
 import { COLORS } from '../../styles/variables'
 import useSiteMetaData from '../../hooks/useSiteMetaData'
+import ProfileImage from '../../components/ProfileImage'
+import ToggleSwitch from '../../components/ToggleSwitch'
+import { AboutMeViewsArr } from '../../types'
+
+const VIEWS: AboutMeViewsArr = ['bio', 'terminal']
 
 const AboutMeMain = () => {
   const PAGE_TITLE = `about me`
   const { terminalCommands } = useSiteMetaData()
+  const [currentView, setCurrentView] = useState<string>(VIEWS[0])
 
   const renderTerminalHeader = () => (
     <Styled.TerminalHeader>
@@ -28,23 +34,38 @@ const AboutMeMain = () => {
     </Styled.TerminalHeader>
   )
 
+  const renderTerminal = () => (
+    <Styled.TerminalContainer>
+      {renderTerminalHeader()}
+      <Styled.TerminalBody>
+        {terminalCommands.map(command => (
+          <TerminalCommand
+            key={`terminal_command_${command.question}`}
+            answer={command.answer}
+            href={command.href}
+            question={command.question}
+          />
+        ))}
+        <TerminalCommand />
+      </Styled.TerminalBody>
+    </Styled.TerminalContainer>
+  )
+
+  const renderInfo = () => (
+    <div>
+      <ProfileImage />
+    </div>
+  )
+
   return (
     <Page id="about-me" css={Styled.PageStyles}>
       <h1 css={Styled.PageHeading}>{PAGE_TITLE.toUpperCase()}</h1>
-      <Styled.TerminalContainer>
-        {renderTerminalHeader()}
-        <Styled.TerminalBody>
-          {terminalCommands.map(command => (
-            <TerminalCommand
-              key={`terminal_command_${command.question}`}
-              answer={command.answer}
-              href={command.href}
-              question={command.question}
-            />
-          ))}
-          <TerminalCommand />
-        </Styled.TerminalBody>
-      </Styled.TerminalContainer>
+      <ToggleSwitch
+        currentView={currentView}
+        setView={setCurrentView}
+        views={VIEWS}
+      />
+      {currentView === 'bio' ? renderInfo() : renderTerminal()}
     </Page>
   )
 }
