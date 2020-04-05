ab21af5287c3db607416037f81a479160f2b4025
Narin Sundarabhaya <nrs710@gmail.com>
2020-04-04 23:44:24 -0700

    refactor(Skills): page layout and mobile styling

--- src/containers/Skills/Skills.styles.ts
+++ src/containers/Skills/Skills.styles.ts
@@ -1,14 +1,21 @@
 import styled from '@emotion/styled'
 import { css } from '@emotion/core'
-import { COLORS } from '../../styles/variables'
+import { BREAKPOINTS, COLORS } from '../../styles/variables'
 
 const SkillsWrapper = styled.div`
   display: grid;
+  grid-row-gap: 1rem;
   grid-template-columns: max-content 1fr;
   grid-template-rows: repeat(2, max-content);
   margin: 0 10%;
   text-align: center;
   width: 100%;
+
+  @media (max-width: ${BREAKPOINTS.mdRem}) {
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(2, max-content);
+    margin: 0 0;
+  }
 `
 
 const PageHeading = css`
@@ -16,18 +23,23 @@ const PageHeading = css`
 `
 
 const PageTitle = css`
+  color: ${COLORS.gray.calm};
   fontsize: 3rem;
+  grid-column: 1 / -1;
   text-align: left;
   text-decoration: underline;
-  grid-column: 1 / -1;
 `
 
 const SelectedSkillsWrapper = styled.div`
-  max-width: 600px;
   display: grid;
+  grid-row-gap: 1rem;
   grid-template-columns: 1fr;
-  grid-template-rows: auto;
-  grid-row-gap: 10px;
+  height: 10.1875rem;
+  max-width: 37.5rem;
+
+  @media (max-width: ${BREAKPOINTS.mdRem}) {
+    height: unset;
+  }
 `
 
 export default {
