be822b8b3122d7c8e7cdb3ce8b86d374fa3d01ce
Narin Sundarabhaya <nrs710@gmail.com>
2020-04-09 23:15:21 -0700

    chore(TageItem): sort css

--- src/components/TagItem/TagItem.Styles.ts
+++ src/components/TagItem/TagItem.Styles.ts
@@ -3,20 +3,19 @@ import { COLORS } from '../../styles/variables'
 
 const ItemWrapper = styled.div`
   display: grid;
+  border: 0.3125rem solid transparent;
   grid-template-columns: max-content;
   grid-template-rows: max-content;
   max-width: 100%;
-  border-left: 0.3125rem solid transparent;
-  border-right: 0.3125rem solid transparent;
 `
 
 const Item = styled.div`
-  padding: 0.3125rem 0.3125rem;
   background-color: ${COLORS.palette.mediumSlateBlue};
+  border-radius: 0.3125rem;
   color: white;
   font-size: 0.825rem;
+  padding: 0.3125rem 0.3125rem;
   width: max-content;
-  border-radius: 0.3125rem;
 `
 
 export default {
