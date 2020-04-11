a72fe86ee26f6991420156163a8b44c4977f48fb
Narin Sundarabhaya <nrs710@gmail.com>
2020-04-10 17:53:00 -0700

    refactor(Page): tablet and mobile spacing

--- src/components/Page/Page.styles.ts
+++ src/components/Page/Page.styles.ts
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled'
-import { DIMENSIONS } from '../../styles/variables'
+import { BREAKPOINTS, DIMENSIONS } from '../../styles/variables'
 
 const StyledPage = styled.div`
   display: flex;
@@ -9,8 +9,12 @@ const StyledPage = styled.div`
   justify-content: center;
   margin: 0 10%;
 
-  @media (max-width: 46.875rem) {
+  @media (max-width: ${BREAKPOINTS.mdRem}) {
     height: 100% !important;
+  }
+
+  @media (max-width: ${BREAKPOINTS.smRem}) {
+    margin: 0 0;
   }
 `
 
