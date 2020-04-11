d96b4326e08f9f32983e290342f72b36ae6fc833
Narin Sundarabhaya <nrs710@gmail.com>
2020-04-10 17:52:04 -0700

    refactor(GalleryItem): mobile and table styling

--- src/components/GalleryItem/Gallery.styles.ts
+++ src/components/GalleryItem/Gallery.styles.ts
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled'
-import { COLORS } from '../../styles/variables'
+import { BREAKPOINTS, COLORS } from '../../styles/variables'
 import { css } from '@emotion/core'
 
 const ExperienceItemWrapper = styled.div`
@@ -7,6 +7,10 @@ const ExperienceItemWrapper = styled.div`
   max-width: 46.875rem;
 
   // custom width to break on desktop when gets close to Gallery Image
+  @media (max-width: 74.375rem) {
+    max-width: 32rem;
+  }
+
   @media (max-width: 66.1875rem) {
     max-width: 25rem;
   }
@@ -37,18 +41,31 @@ const ExperienceTitleRow = styled.div`
   &:last-child {
     justify-self: end;
   }
+
+  @media (max-width: ${BREAKPOINTS.smRem}) {
+    max-width: calc(100vw - 5rem);
+    line-break: auto;
+  }
 `
 
 const ExperienceMyTitleRow = styled.div`
   display: grid;
   grid-template-columns: max-content max-content;
   grid-column-gap: 0.5rem;
+
+  @media (max-width: ${BREAKPOINTS.smRem}) {
+    max-width: calc(100vw - 5rem);
+  }
 `
 
 const ExperienceTitle = styled.div`
   font-size: 1.5rem;
   font-weight: bold;
   color: ${COLORS.gray.copy};
+
+  @media (max-width: ${BREAKPOINTS.smRem}) {
+    max-width: calc(100vw - 6.9375rem);
+  }
 `
 
 const ExperienceMyTitle = styled.div`
