05cc710275a536d5697e4e27bd30f6e56e7090f6
Narin Sundarabhaya <nrs710@gmail.com>
2020-04-10 17:54:02 -0700

    refactor(Contact): new layout and get items from meta data

--- src/containers/Contact/index.tsx
+++ src/containers/Contact/index.tsx
@@ -1,56 +1,50 @@
-import React from 'react'
 import Page from '../../components/Page'
-import Container from '../../components/Container'
+import PageHeading from '../../components/PageHeading'
+import React from 'react'
+import ScrollReveal from '../../components/ScrollReveal'
 import Styled from './Contact.styles'
+import useSiteMetaData from '../../hooks/useSiteMetaData'
 
-// Assets
-import github from '../../images/github.svg'
-import twitter from '../../images/circle.svg'
-import insta from '../../images/insta.svg'
-import linkedin from '../../images/in.svg'
+const HEADING = `Contact`
 
-const SkillsContainer = () => (
-  <Page id="contact" css={Styled.PageHeading}>
-    <Styled.ContactWrapper>
-      <Container>
-        <h1 css={Styled.PageTitle}>
-          <a css={Styled.Anchor} href="mailto:nrs710@gmail.com" target="__top">
-            {`get in touch`.toUpperCase()}
-          </a>
-        </h1>
-        <Styled.ContactRow>
-          <a
-            href="https://github.com/ricopella"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Styled.Image src={github} alt="Github - @ricopella" />
-          </a>
-          <a
-            href="https://www.instagram.com/ricopella"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Styled.Image src={insta} alt="Instagram - @ricopella" />
-          </a>
-          <a
-            href="https://twitter.com/ricopella"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Styled.Image src={twitter} alt="Twitter - @ricopella" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/nsundara/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Styled.Image src={linkedin} alt="LinkedIn - Narin Sundarabhaya" />
-          </a>
-        </Styled.ContactRow>
-      </Container>
-    </Styled.ContactWrapper>
-  </Page>
-)
+const SkillsContainer = () => {
+  const { contactItems } = useSiteMetaData()
+
+  const renderContactItems = () => (
+    <Styled.ContactRow>
+      {contactItems.map((item, idx) => (
+        <Styled.ContactLink
+          key={`contact_items_${item.title}_${idx}`}
+          href={item.href}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {`+ ${item.title}`}
+        </Styled.ContactLink>
+      ))}
+    </Styled.ContactRow>
+  )
+
+  return (
+    <ScrollReveal>
+      <Page id="contact" css={Styled.PageHeading}>
+        <Styled.ContactWrapper>
+          <PageHeading title={HEADING} />
+          <Styled.ContactDescription>
+            Whether you're a company looking to add to your team, a business
+            owner looking build an app or web site with a freelance web
+            developer, or a developer looking to collaborate on a project, or
+            just wanting to say hi and see a picture of my dog, shoot me an{' '}
+            <Styled.ContactLink href="mailto:nrs710@gmail.com" target="__top">
+              email
+            </Styled.ContactLink>{' '}
+            and let's build something great together!
+          </Styled.ContactDescription>
+          {renderContactItems()}
+        </Styled.ContactWrapper>
+      </Page>
+    </ScrollReveal>
+  )
+}
 
 export default SkillsContainer
