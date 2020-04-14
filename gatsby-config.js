b834a6d2dcb26aad293d4eb4c18d94608aef7cbe
Narin Sundarabhaya <nrs710@gmail.com>
2020-04-13 22:48:40 -0700

    add: .env for google analytics id

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,12 @@
+const activeEnv =
+    process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+
+console.log(`Using environment config: '${activeEnv}'`)
+
+require("dotenv").config({
+    path: `.env.${activeEnv}`,
+})
+
 module.exports = {
     siteMetadata: {
         title: `Welcome`,
@@ -409,5 +418,15 @@ module.exports = {
         `gatsby-plugin-typescript`,
         `gatsby-plugin-scroll-reveal`,
         'gatsby-plugin-anchor-links',
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: process.env.GA_TRACKING_ID || "",
+                // Setting this parameter is optional
+                anonymize: true,
+                // Setting this parameter is also optional
+                respectDNT: true,
+            },
+        },
     ],
 }
