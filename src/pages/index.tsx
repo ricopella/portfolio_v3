5d389c2b93bca958abe60f12216180cc9a9cb3c1
Narin Sundarabhaya <nrs710@gmail.com>
2020-04-16 23:58:30 -0700

    ✨ (index): add ThemeContext

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,24 @@
-import Layout from '../components/Layout/index'
-import Main from '../containers/Main/index'
-import React from 'react'
-import SEO from '../components/SEO/index'
+import Layout from '../components/Layout'
+import Main from '../containers/Main'
+import React, { useState } from 'react'
+import SEO from '../components/SEO'
+import ThemeContext from '../providers/ThemeContext'
 import { DARK_MODE_THEME, LIGHT_MODE_THEME } from '../styles/variables'
 import { ThemeProvider } from 'emotion-theming'
 
-const IndexPage = () => (
-  <ThemeProvider theme={DARK_MODE_THEME}>
-    <Layout>
-      <SEO />
-      <Main />
-    </Layout>
-  </ThemeProvider>
-)
+const IndexPage = () => {
+  const [isThemeDark, setIsThemeDark] = useState<boolean>(true)
+
+  return (
+    <ThemeContext.Provider value={{ isThemeDark, setIsThemeDark }}>
+      <ThemeProvider theme={isThemeDark ? DARK_MODE_THEME : LIGHT_MODE_THEME}>
+        <Layout>
+          <SEO />
+          <Main />
+        </Layout>
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  )
+}
 
 export default IndexPage
