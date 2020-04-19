45e5f5ee3e93cd16db1cc4e996bd1a31aa2a58f8
Narin Sundarabhaya <nrs710@gmail.com>
2020-04-19 00:46:02 -0700

    ✨ (GalleryItem): add donut chart and github hook

--- src/components/GalleryItem/index.tsx
+++ src/components/GalleryItem/index.tsx
@@ -1,6 +1,9 @@
+import DonutChart from '../DonutChart'
+import format from 'date-fns/format'
 import Image from '../GalleryImage'
 import LaunchArrow from '../LaunchArrow'
 import React, { FC } from 'react'
+import setGithubLanguageData from '../../utils/setGithubLanguageData'
 import Styled from './Gallery.styles'
 import TagItem from '../TagItem'
 import { GalleryProps } from '../../types'
@@ -11,45 +14,66 @@ const GalleryItem: FC<GalleryProps> = ({
   filename,
   href,
   myTitle,
+  github = null,
   tech,
   title,
   year,
-}) => (
-  <Styled.ExperienceItemWrapper>
-    <Styled.GalleryImageWrapper>
-      {filename ? (
-        <Image filename={filename} css={Styled.GalleryImage} alt={alt} />
-      ) : (
-        <div />
-      )}
-    </Styled.GalleryImageWrapper>
-    <Styled.ExperienceContentWrapper>
-      <Styled.ExperienceTitleRow>
-        <Styled.ExperienceTitle>{title}</Styled.ExperienceTitle>
-        {href ? <LaunchArrow href={href} /> : null}
-      </Styled.ExperienceTitleRow>
-      <Styled.ExperienceMyTitleRow>
-        {myTitle ? (
-          <Styled.ExperienceMyTitle>{myTitle}</Styled.ExperienceMyTitle>
-        ) : null}
-        {year ? (
-          <Styled.ExperienceMyTitle>
-            {myTitle ? '|' : 'Circa:'} {year}
-          </Styled.ExperienceMyTitle>
+}) => {
+  let githubData = null
+
+  if (github) {
+    githubData = setGithubLanguageData(github?.languages)
+  }
+
+  return (
+    <Styled.ExperienceItemWrapper>
+      <Styled.GalleryImageWrapper>
+        {filename ? (
+          <Image filename={filename} css={Styled.GalleryImage} alt={alt} />
         ) : (
           <div />
         )}
-      </Styled.ExperienceMyTitleRow>
-      <p>{description}</p>
-      <Styled.TagItemsWrapper>
-        {tech && tech.length
-          ? tech.map((item: string, idx: number) => (
-              <TagItem key={`tag_item_${item}_${idx}`} title={item} />
-            ))
-          : null}
-      </Styled.TagItemsWrapper>
-    </Styled.ExperienceContentWrapper>
-  </Styled.ExperienceItemWrapper>
-)
+      </Styled.GalleryImageWrapper>
+      <Styled.ExperienceContentWrapper>
+        <Styled.ExperienceTitleRow>
+          <Styled.ExperienceTitle>{title}</Styled.ExperienceTitle>
+          {github?.url || href ? (
+            <LaunchArrow href={github?.url || href} />
+          ) : null}
+        </Styled.ExperienceTitleRow>
+        <Styled.ExperienceMyTitleRow>
+          {myTitle ? (
+            <Styled.ExperienceMyTitle>{myTitle}</Styled.ExperienceMyTitle>
+          ) : null}
+          {year ? (
+            <Styled.ExperienceMyTitle>
+              {myTitle ? '|' : github?.createdAt ? 'Created:' : 'Circa:'}{' '}
+              {github?.createdAt
+                ? format(new Date(github?.createdAt), 'MM dd yyyy')
+                : year}{' '}
+              {github?.updatedAt
+                ? `Last Updated: ${format(
+                    new Date(github?.updatedAt),
+                    'MM dd yyyy'
+                  )}`
+                : ''}
+            </Styled.ExperienceMyTitle>
+          ) : (
+            <div />
+          )}
+        </Styled.ExperienceMyTitleRow>
+        <p>{description}</p>
+        {github ? <DonutChart data={githubData} /> : null}
+        <Styled.TagItemsWrapper>
+          {tech && tech.length
+            ? tech.map((item: string, idx: number) => (
+                <TagItem key={`tag_item_${item}_${idx}`} title={item} />
+              ))
+            : null}
+        </Styled.TagItemsWrapper>
+      </Styled.ExperienceContentWrapper>
+    </Styled.ExperienceItemWrapper>
+  )
+}
 
 export default GalleryItem
