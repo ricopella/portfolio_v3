df59109f5b0b96604b5495324b7799ee5a2acd8d
Narin Sundarabhaya <nrs710@gmail.com>
2020-04-21 21:54:57 -0700

    ✅ (setCurentSkills): add unit test and docs

--- src/utils/setCurrentSkills.ts
+++ src/utils/setCurrentSkills.ts
@@ -1,4 +1,8 @@
 import { Skills, SubSkills } from '../types'
 
+/**
+ * Utility function that list of categories (Languages, Front-End, Back-End, ....)
+ * and returns it's children (skills in that category)
+ */
 export default (categories: Skills[], selectedCategory: string): SubSkills[] =>
   categories.find(category => category.title === selectedCategory)?.items || []
