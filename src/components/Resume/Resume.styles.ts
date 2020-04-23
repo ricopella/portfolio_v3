8dd727700c484350551d0ce8edf32326a1fcaa22
Narin Sundarabhaya <nrs710@gmail.com>
2020-04-22 23:02:53 -0700

    💄 (Resume): layout of resume body

--- src/components/Resume/Resume.styles.ts
+++ src/components/Resume/Resume.styles.ts
@@ -1,5 +1,8 @@
 import styled from '@emotion/styled'
-import { BREAKPOINTS, COLORS } from '../../styles/variables'
+import { BREAKPOINTS, DIMENSIONS } from '../../styles/variables'
+import { css } from '@emotion/core'
+import { Link } from 'gatsby'
+import { OutboundLink } from 'gatsby-plugin-google-analytics'
 import { ThemeTypes } from '../../types'
 
 const ResumeContainer = styled.div`
@@ -13,12 +16,17 @@ const ResumeWrapper = styled.div`
     props.theme.backgroundContentColor};
   border-radius: 0.3125rem;
   box-shadow: 0 -1px 0.625rem rgba(0, 0, 0, 0.75);
+  display: grid;
+  grid-row-gap: 1rem;
+  grid-template-columns: 1fr;
+  grid-template-rows: ${DIMENSIONS.resume.heading.height} 1fr;
+  height: 297mm;
   margin: 6rem auto;
   min-height: 100vh;
   padding: 2rem 0;
-  width: 66.4375rem;
+  width: 210mm;
 
-  @media (max-width: 1150px) {
+  @media (max-width: 71.875rem) {
     margin: 6rem auto;
     width: calc(100vw - 4rem);
   }
@@ -28,10 +36,95 @@ const ResumeWrapper = styled.div`
   }
 `
 
+const ResumeBody = styled.div`
+  display: grid;
+  grid-row: 2;
+  grid-column: 1/-1;
+  grid-template-columns: 1fr 2fr;
+  grid-template-rows: repeat(2, 1fr);
+  grid-template-areas:
+    'contact workExperience'
+    'skills education';
+`
+
+const ResumeBodyHeading = styled.div`
+  background-color: ${(props: ThemeTypes) => props.theme.actionBackgroundColor};
+  height: 2rem;
+  width: 80%;
+  color: ${(props: ThemeTypes) => props.theme.colorCalm};
+  padding: 0 0 0 1rem;
+  line-height: 2rem;
+`
+
+// Contact Info
+const Contact = styled.div`
+  display: grid;
+  width: 12.5rem;
+  grid-area: contact;
+  border-right: 0.125rem solid
+    ${(props: ThemeTypes) => props.theme.actionBackgroundColor};
+  grid-template-rows: repeat(2, max-content);
+  grid-row-gap: 1rem;
+`
+
+const ContactBody = styled.div`
+  display: grid;
+  grid-template-rows: repeat(6, max-content);
+  grid-row-gap: 1rem;
+`
+
+const ContactRowItem = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(2, max-content);
+  padding-left: 1rem;
+`
+
+const ContactRowKey = styled.div`
+  color: ${(props: ThemeTypes) => props.theme.color};
+  font-size: 0.875rem;
+`
+
+const ContactRowValue = styled.div`
+  color: ${(props: ThemeTypes) => props.theme.color};
+  font-size: 0.875rem;
+`
+
+const ContactRowValueLink = styled(OutboundLink)`
+  font-size: 0.875rem;
+`
+
+const ContactRowValueInternalLink = styled(Link)`
+  font-size: 0.875rem;
+`
+
+const Skills = styled.div`
+  display: grid;
+  width: 20%;
+  grid-area: skills;
+  border-right: 0.125rem solid
+    ${(props: ThemeTypes) => props.theme.actionBackgroundColor};
+  width: 12.5rem;
+`
+
+const WorkExperience = styled.div`
+  display: grid;
+  width: 60%;
+  grid-area: workExperience;
+`
+
+const Education = styled.div`
+  display: grid;
+  width: 60%;
+  grid-area: education;
+`
+
+// HEADING
 const Heading = styled.div`
   display: grid;
   position: relative;
   width: 100%;
+  grid-row: 1;
 `
 
 const HeadingProfileImageWrapper = styled.div`
@@ -45,16 +138,28 @@ const HeadingProfileImageWrapper = styled.div`
   }
 `
 
+const HeadingProfileImageOuter = css`
+  height: ${DIMENSIONS.resume.heading.height};
+  width: ${DIMENSIONS.resume.heading.height};
+`
+
+const HeadingProfileImageInner = css`
+  height: calc(${DIMENSIONS.resume.heading.height} - 1rem) !important;
+  width: calc(${DIMENSIONS.resume.heading.height} - 1rem) !important;
+`
+
 const HeadingWrapper = styled.div`
   display: grid;
+  background: rgb(76, 73, 97);
   grid-template-columns: 1fr;
-  grid-template-rows: 1fr 1fr;
-  height: 12.5rem;
-  padding: 4rem 0 2rem 10rem;
+  grid-template-rows: repeat(2, 1.5rem);
+  grid-row-gap: 1.5rem;
+  height: ${DIMENSIONS.resume.heading.height};
+  padding: 0 0 0 ${DIMENSIONS.resume.heading.height};
+  align-content: center;
   position: absolute;
   right: 0;
-  width: 50rem;
-  background: rgb(76, 73, 97);
+  width: 40rem;
   background: -moz-linear-gradient(
     308deg,
     rgba(76, 73, 97, 0.7721463585434174) 10%,
@@ -72,12 +177,12 @@ const HeadingWrapper = styled.div`
   );
   filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#4c4961",endColorstr="#373737",GradientType=1);
 
-  @media (max-width: 1150px) {
+  @media (max-width: 71.875rem) {
     width: 75%;
   }
 
   @media (max-width: 47.9375rem) {
-    height: 10rem;
+    height: ${DIMENSIONS.resume.heading.height};
     padding: 3rem 0 2rem 6rem;
   }
 
@@ -93,7 +198,7 @@ const HeadingWrapper = styled.div`
 
 const HeadingText = styled.div`
   font-size: 3rem;
-  color: ${COLORS.white};
+  color: ${(props: ThemeTypes) => props.theme.colorCalm};
 
   &.first {
     font-size: 4rem;
@@ -121,4 +226,18 @@ export default {
   HeadingText,
   HeadingWrapper,
   HeadingProfileImageWrapper,
+  Contact,
+  Skills,
+  WorkExperience,
+  Education,
+  ResumeBody,
+  HeadingProfileImageInner,
+  HeadingProfileImageOuter,
+  ResumeBodyHeading,
+  ContactRowItem,
+  ContactRowKey,
+  ContactRowValue,
+  ContactBody,
+  ContactRowValueLink,
+  ContactRowValueInternalLink,
 }
