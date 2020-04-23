263fcca9b08c2c88f9b0d81b1d45a80ad25fe175
Narin Sundarabhaya <nrs710@gmail.com>
2020-04-22 18:16:09 -0700

    ♻️ (SlideUpElement): add style prop

--- src/components/SlideUpElement/index.tsx
+++ src/components/SlideUpElement/index.tsx
@@ -1,9 +1,14 @@
-import React, { FC } from 'react'
+import React, { CSSProperties, FC } from 'react'
 import { motion } from 'framer-motion'
 import { SLIDE_IN_ANIMATION_OPTIONS } from '../../styles/variables'
 
-const SlideUpElement: FC<{}> = ({ children }) => (
-  <motion.div {...SLIDE_IN_ANIMATION_OPTIONS}>{children}</motion.div>
+const SlideUpElement: FC<{ style?: CSSProperties }> = ({
+  children,
+  style = {},
+}) => (
+  <motion.div style={style} {...SLIDE_IN_ANIMATION_OPTIONS}>
+    {children}
+  </motion.div>
 )
 
 export default SlideUpElement
